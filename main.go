c53bd6939206600e83ea3ac2ef3b7e9d1bda194d
yhl <yehaolongcs@163.com>
2023-08-19 11:46:00 +0800

    所有结构改造完成，正常运行

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"bubble/controller"
 	"bubble/dao"
-	"github.com/gin-gonic/gin"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"bubble/models"
+	"bubble/routers"
 )
 
 func main() {
@@ -17,31 +16,7 @@ func main() {
 	}
 	defer dao.Close() //程序退出关闭数据库连接
 	//模型绑定
-	dao.DB.AutoMigrate(&Todo{})
-	r := gin.Default()
-	//告诉gin框架模板文件引用的静态文件去哪里找
-	r.Static("/static", "static")
-	// 告诉gin框架去哪里找模板文件
-	r.LoadHTMLGlob("templates/*")
-	r.GET("/", controller.IndexHandler)
+	dao.DB.AutoMigrate(&models.Todo{})
+	r := routers.SetupRouter()
 	r.Run()
-	//v1版本代码
-
-	v1Group := r.Group("v1")
-	{
-		//待办事项
-		//添加
-		v1Group.POST("/todo", controller.CreateATodo)
-		//查看所有的待办事项
-		v1Group.GET("/todo", controller.GetTodoList)
-		//查看某一个代办事项
-		//v1Group.GET("/todo/:id", func(c *gin.Context) {
-		//
-		//})
-		//修改
-		v1Group.PUT("/todo/:id", controller.UpdateATodo)
-		//删除
-		v1Group.DELETE("/todo/:id", controller.DeleteATodo)
-	}
-
 }
--- models/todo.go
+++ models/todo.go
@@ -37,3 +37,7 @@ func UpdateATodo(todo *Todo) (err error) {
 	err = dao.DB.Save(todo).Error
 	return
 }
+func DeleteATodo(id string) (err error) {
+	err = dao.DB.Where("id=?", id).Delete(&Todo{}).Error
+	return
+}
